675d088ee9c8a50efc4ea61c9e2fe88ffdff5dee
Alex Martin <alex.pmartin1@gmail.com>
2021-10-04 19:59:59 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun(phrase1 = `roller-skate`){
+    return `This Saturday, I want to ${phrase1}!`
+}
+
+function mondayWork(phrase2 = `go to the office`){
+    return `This Monday, I will ${phrase2}.`
+}
+
+function wrapAdjective(phrase3 = `*`){
+    return function(phrase4 = `special`){
+        return `You are ${phrase3}${phrase4}${phrase3}!`
+    }
+}
